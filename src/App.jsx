77b29a45a70a04943a9d44badae73f0b698affd9
juniaaaa133 <xopeofhopeness2004@gmail.com>
2024-05-21 20:04:26 +0630

    Solved Task 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import './index.css'
+import './style.css'
 
 function App() {
+
+let [data,setData] = useState([]);
 
 let fetchData = async () => {
 try {
   let res = await fetch('https://jsonplaceholder.typicode.com/todos')
 let data = await res.json();
-console.log(data);
+setData(data);
 } catch (error) {
   console.log(error);
 }
@@ -17,7 +21,26 @@ useEffect(()=>{
 },[])
 
   return (
-   <div>Hey There!</div>
+  <>
+  <div className="title main-f" style={{
+    textAlign : "center"
+  }}>ToDos</div>
+  <div className="list-ctn">
+  {
+    data.map((list,index) => (
+      <div key={index} className="lisT">
+        {
+          list.completed ? 
+          <p className="active main-f">Completed</p>
+          :
+          <p className="ongoin main-f">Ongoing</p>
+        }
+      <h4 className="list-title main-f">{list.title}</h4>
+    </div>
+    ))
+  }
+  </div>
+  </>
   )
 }
 
